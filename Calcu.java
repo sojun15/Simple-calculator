3926e87c420def70c18b71dee6a881740c15c39d
sojun15 <sojun2115@cseku.ac.bd>
2024-05-01 16:23:27 -0700

    Normal operation can perform

---
+++ Calcu.java
@@ -0,0 +1,9 @@
+package com.mycompany.calcu;
+
+public class Calcu {
+    public static void main(String[] args){
+        Form c = new Form();
+        c.setVisible(true);
+        c.setBounds(300, 150, 300, 400); 
+    }
+}
